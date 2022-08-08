b23101d7c24627df502268dbafe38fb34b3c1b85
Tommy Rogers <tomrog428@yahoo.com>
2022-08-08 16:42:17 -0600

    change user model

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -4,5 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    subscribe_newsletters = models.BooleanField(default=True)
+    old_id = models.IntegerField(null=True, blank=True)
+    old_source = models.CharField(max_length=25, null=True, blank=True)
