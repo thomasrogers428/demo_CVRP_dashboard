6776a29b798ab34f210c8f76aef57a4c73035bf7
Tommy Rogers <tomrog428@yahoo.com>
2022-08-10 15:38:45 -0600

    bug fix

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'home/trogers/db.sqlite3',
+            'NAME': '/home/trogers/db.sqlite3',
         }
     }
 
